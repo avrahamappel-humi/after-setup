a9597cf994e448a5ee439e9f7f94a392088c4745
Avraham Appel <avraham.appel@humi.ca>
2023-01-24 12:37:11 -0500

    Remove cruft

--- shells/payroll.nix
+++ shells/payroll.nix
@@ -3,29 +3,14 @@
 let
   env = pkgs.bundlerEnv {
     name = "payroll-bundler-env";
-    # ruby = pkgs.ruby_3_0;
     ruby = pkgs.ruby_3_1;
-    # inherit (pkgs) ruby;
     gemdir = "${builtins.getEnv "HOME"}/humility/applications/payroll";
     ignoreCollisions = true;
   };
 in
 pkgs.mkShell {
-  # nativeBuildInputs = with pkgs; [
-  #   # Required for building postgres gem
-  #   postgresql
-  # ];
-
   buildInputs = with pkgs; [
-    # (pkgs.lib.lowPrio env)
     env
     env.wrappedRuby
-    # ruby_3_1
-    # rubyPackages_3_1.solargraph
   ];
-
-  # shellHook = ''
-  #   # Required for building thin gem
-  #   bundle config build.thin -fdeclspec
-  # '';
 }
