feec1cbb1e2da4c9e199e84f6beb18bf8323d099
Avraham Appel <avraham.appel@humi.ca>
2023-01-24 22:54:44 -0500

    try to use ruby-nix

--- shells/payroll.nix
+++ shells/payroll.nix
@@ -1,16 +1,39 @@
 { pkgs ? import <nixpkgs> { } }:
 
 let
-  env = pkgs.bundlerEnv {
+  bundix = (pkgs.fetchFromGitHub {
+    owner = "sagittaros";
+    repo = "bundix";
+    rev = "4e0d18a";
+    sha256 = "sha256-KDKdesIUpyM6YnGsZMOxN34NhTNCsXvd03qsHNtUdWc=";
+  });
+
+  rubyNixBuilder = import
+    (pkgs.fetchFromGitHub {
+      owner = "sagittaros";
+      repo = "ruby-nix";
+      rev = "b4f3bc5";
+      sha256 = "sha256-cx9E+VEUmoiRcaANkECzZKZQyj3IG5YMntZMRzbvHQ4=";
+    })
+    bundix;
+
+
+  rubyNix = rubyNixBuilder (pkgs // {
+    buildEnv = args: pkgs.buildEnv (args // {
+      ignoreCollisions = true;
+    });
+  });
+
+  env = rubyNix {
     name = "payroll-bundler-env";
     ruby = pkgs.ruby_3_1;
-    gemdir = "${builtins.getEnv "HOME"}/humility/applications/payroll";
-    ignoreCollisions = true;
+    gemset = "${builtins.getEnv "HOME"}/humility/applications/payroll/gemset.nix";
   };
 in
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    env
-    env.wrappedRuby
+  buildInputs = with env; [
+    # env
+    envMinimal
+    # ruby
   ];
 }
